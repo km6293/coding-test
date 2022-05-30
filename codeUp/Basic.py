1ea162663579bce4d9d9253f7d1d5df6e9c4b20e
alstn7616@naver.com <alstn7616@naver.com>
2022-05-30 21:38:59 +0900

    codeUp 기초

--- codeUp/Basic.py
+++ codeUp/Basic.py
@@ -1,1 +1,57 @@
-print("hello world")
+# 6044 합, 차, 곱, 몫, 나머지, format
+# a,b = input().split()
+# print(int(a)+int(b))
+# print(int(a)-int(b))
+# print(int(a)*int(b))
+# print(int(a)//int(b))
+# print(int(a)%int(b))
+# print(format(int(a)/int(b), ".2f"))
+
+
+#6045 세 정수
+# a,b,c = input().split()
+# print(int(a)+int(b)+int(c) , format((int(a)+int(b)+int(c))/3, ".2f"))
+
+
+#6046 비트연산자 
+# a = int(input())
+# print(a << 1)
+
+#6047 비트연산자 2의 n승..
+# a,b = input().split()
+# print(int(a) << int(b))
+
+#6048 삼항연산자
+# a, b = input().split()
+# print('True') if int(a) < int(b) else print('False')
+
+#6049 
+# a, b = input().split()
+# print('True') if int(a) == int(b) else print('False')
+
+#6050
+# a, b = input().split()
+# print('True') if int(a) <= int(b) else print('False')
+
+#6051
+# a, b = input().split()
+# print(a != b)
+
+#6052
+# print(bool(int(input())))
+
+#6053
+# print(not bool(int(input())))
+
+#6054
+# a, b = input().split()
+# print(bool(int(a)) and bool(int(b)))
+
+#6055
+# a, b = input().split()
+# print(bool(int(a)) or bool(int(b)))
+
+#6056
+
+
+
