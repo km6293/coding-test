e54b1c137a7d5977018f3ef332b720a81affd6e5
alstn7616@naver.com <alstn7616@naver.com>
2022-06-01 21:45:37 +0900

    codeUp 기초

--- codeUp/Basic.py
+++ codeUp/Basic.py
@@ -52,6 +52,119 @@
 # print(bool(int(a)) or bool(int(b)))
 
 #6056
+# a, b = input().split()
+# print(not a == b)
 
+#6057
+# a, b = input().split()
+# print(a == b)
 
+#6058
+# a, b = input().split()
+# print(bool(int(a)) == False and bool(int(b)) == False)
+
+#6059
+# print(~int(input()))
+
+#6060
+# a, b = input().split()
+# print(int(a) & int(b))
+
+#6061
+# a, b = input().split()
+# print(int(a) | int(b))
+
+#6062
+# a, b = input().split()
+# print(int(a) ^ int(b))
+
+#6063
+# a, b = input().split()
+# print(int(a) if int(a) > int(b) else int(b))
+
+#6064
+# a, b, c = input().split()
+# print( int(a) if int(a) < int(b) and int(a) < int(c) else int(b) if int(b) < int(c) else int(c) )
+
+#6065
+# a, b, c = input().split()
+# if int(a) % 2 == 0 : print(a)
+# if int(b) % 2 == 0 : print(b)
+# if int(c) % 2 == 0 : print(c)
+
+# 또 다른 풀이
+# s = map(int, input().split())
+# for i in s:
+#   if i % 2 == 0:
+#     print(i)
+
+#6066
+# s = map(int, input().split())
+# for i in s:
+#   if(i % 2) == 0:
+#     print('even')
+#   else:
+#     print('odd')
+
+#6067
+# s = map(int, input().split())
+# for i in s:
+#   if i < 0:
+#     if(i % 2) == 0:
+#       print('A')
+#     else:
+#       print('B')
+#   else :
+#     if(i % 2) == 0:
+#       print('C')
+#     else:
+#       print('D')
+
+#6068
+# a = int(input())
+# if a >= 90:
+#   print("A")
+# else :
+#   if a >= 70:
+#     print("B")
+#   else :
+#     if a >= 40:
+#       print("C")
+#     else :
+#       print("D")
+    
+#6069
+# a = input()
+# if a == 'A':
+#   print('best!!!')
+# else :
+#   if a == 'B':
+#     print('good!!')
+#   else:
+#     if a == 'C':
+#       print('run!')
+#     else:
+#       if a == 'D':
+#         print('slowly~')
+#       else:
+#         print('what?')
+
+#6070
+# a = int(input())
+# if a // 3 == 1:
+#   print('spring')
+# elif a // 3 == 2:
+#   print('summer')
+# elif a // 3 == 3:
+#   print('fall')
+# else:
+#   print('winter')
+
+#6071
+# num = int(input())
+# while num != 0:
+#   print(num)
+#   num = int(input())
+
+#6072
 
