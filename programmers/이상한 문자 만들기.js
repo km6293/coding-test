1dd38843861e32f754efc616f5d3b11e6d1d7987
km6293 <alstn7616@naver.com>
2022-07-30 20:13:39 +0900

    level1

--- programmers/이상한 문자 만들기.js
+++ programmers/이상한 문자 만들기.js
@@ -1,7 +1,14 @@
 function solution(s) {
-  //return s.split('').map((t,i) => i % 2 ? t.toLowerCase(): t.toUpperCase()).join('')
-  return s.split(' ').map((t,i) => t.split('').map((a,b) => a))
+  return s.split(' ').map((t) => t.split('').map((a,i) => i % 2 ? a.toLowerCase(): a.toUpperCase()).join("")).join(" ")
 }
 
 console.log(solution("try hello world"))
 console.log(solution("try helloworld"))
+
+
+// 좋아요가 많은 풀이
+
+function toWeirdCase(s){
+  //함수를 완성해주세요
+  return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();})
+}
