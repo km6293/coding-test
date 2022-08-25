65cc99cb0bf16e6fb1d9f9528ad7bc3df166b435
km6293 <alstn7616@naver.com>
2022-08-25 23:27:45 +0900

    test

--- baekJoon/사용법.js
+++
@@ -1,8 +0,0 @@
-const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
-
-const [n, m, v] = input[0].split(" ").map(Number);
-const mulitpleInput = input[1].split(" ").map(Number);
