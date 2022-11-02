2331ecc8af8ada6ca7522fa7542dfb0dc7bda9a1
km6293 <alstn7616@naver.com>
2022-11-02 23:34:18 +0900

    소수찾기

---
+++ programmers/소수찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+  for(let i = 2; i * i <= n; i++){
+    if(arr[i]){
+      for(let j = i * i; j <= n; j+=i){
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter(e => e).length
+}
+
+let test = solution(100);
+console.log(test)
+
