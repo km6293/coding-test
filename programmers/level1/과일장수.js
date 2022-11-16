f091f27cc5a545a53fe9a150e1f7941c0a5b9f35
km6293 <alstn7616@naver.com>
2022-11-16 23:33:22 +0900

    level1

--- programmers/level1/과일장수.js
+++ programmers/level1/과일장수.js
@@ -1,7 +1,13 @@
 function solution(k, m, score) {
-  var answer = 0;
-  return answer;
+  let total = 0;
+  let sortArr = [...score].sort((a,b) => b - a);
+  for (let i = 1 ; i < parseInt(score.length / m)+1; i++){  total += sortArr[m*i-1];}
+  return total*m;
 }
+
+console.log(solution(4,3,[4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
+// console.log(solution(3,4,[1, 2, 3, 1, 2, 3, 1]))
+// solution(3,4,[1, 2, 3, 1, 2, 3, 1])
 
 // k	m	score	result
 // 3	4	[1, 2, 3, 1, 2, 3, 1]	8
