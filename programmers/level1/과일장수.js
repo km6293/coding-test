b9d3bdc602f5e55582f22e023bedaee11104aeef
km6293 <alstn7616@naver.com>
2022-11-16 22:50:01 +0900

    정리

---
+++ programmers/level1/과일장수.js
@@ -0,0 +1,8 @@
+function solution(k, m, score) {
+  var answer = 0;
+  return answer;
+}
+
+// k	m	score	result
+// 3	4	[1, 2, 3, 1, 2, 3, 1]	8
+// 4	3	[4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]	33
---
+++ test.js
@@ -0,0 +1,8 @@
+// function solution(numbers) {
+//   return (numbers => numbers.reduce((p, c) => p + c, 0) / numbers.length);
+//}
+let number = [1,2,3,4,5.5]
+const a = (number) => number.reduce((a,b) => a+b, 0) / number.length
+console.log(a)
+
+
