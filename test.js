2af71d7e1bf5754f9caaa0ad6890e6cbdad20517
km6293 <alstn7616@naver.com>
2022-12-05 23:01:40 +0900

    level1

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-// function solution(numbers) {
-//   return (numbers => numbers.reduce((p, c) => p + c, 0) / numbers.length);
-//}
-let number = [1,2,3,4,5.5]
-const a = (number) => number.reduce((a,b) => a+b, 0) / number.length
-console.log(a)
+// const solution = (numbers) => numbers.reduce((a, c) => a += c, 0) / numbers.length
 
+const solution = (arr,h) => arr.filter(e => e > h).length
 
+// console.log(solution([149, 180, 192, 170], 167),	3)
+// console.log(solution([180, 120, 140],190),	0)
+// console.log([1,2,3].map(e => e))
+console.log([3,1,2,3].sort()[0])
