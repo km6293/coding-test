26047d59b37cd63ad9d896c96f0f3e6cf66305f4
km6293 <alstn7616@naver.com>
2022-12-07 20:39:46 +0900

    level1

---
+++ 문제/프로그래머스/level1/신고 결과 받기.js
@@ -0,0 +1,22 @@
+function solution(idList, report, k) {
+  let [arr, chkId] = [[...new Set(report)],[]];
+  let [cntArr,reVal] = [Array(idList.length).fill(0),Array(idList.length).fill(0)];
+  arr.forEach(e => {
+    let deId = e.substring(e.indexOf(' ')+1,e.length)
+    idList.forEach((f,i) => {
+      if(f == deId) cntArr[i] += 1;
+    })
+  })
+  cntArr.forEach((e,i) => { if(e >= k) chkId.push(idList[i]) })
+  arr.forEach(e => {
+    let [useId, deId] = e.split(' ');
+    if(chkId.includes(deId)) {
+      reVal[idList.indexOf(useId)] += 1
+    }
+  })
+  return reVal
+}
+
+
+// console.log(solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2),[2,1,1,0])
+console.log(solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3),[0,0])
