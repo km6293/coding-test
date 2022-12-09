fdfdb17e8c29f53893608d37771213239e8b3df3
km6293 <alstn7616@naver.com>
2022-12-09 20:24:26 +0900

    level1

---
+++ 문제/프로그래머스/level2/이진 변환 반복하기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let [delCnt, chgCnt, zero] = [0,0,0];
+  while(s != 1){
+    for(let i = 0; i < s.length; i++){ 
+      if(!parseInt(s[i])) {
+        delCnt += 1; 
+        zero += 1; 
+      }
+    }
+    s = (s.length - zero).toString(2);
+    chgCnt += 1;
+    zero = 0;
+  }
+  return [chgCnt, delCnt];
+}
+
+
+// console.log(solution("110010101001"),	[3,8])
+// console.log(solution("01110"),	[3,3])
+console.log(solution("1111111"),	[4,1])
