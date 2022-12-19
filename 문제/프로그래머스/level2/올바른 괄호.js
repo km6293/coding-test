2bc1ad5851ac77fed19f51bfdb63045523d5e025
km6293 <alstn7616@naver.com>
2022-12-20 00:33:16 +0900

    level2

--- 문제/프로그래머스/level2/올바른 괄호.js
+++ 문제/프로그래머스/level2/올바른 괄호.js
@@ -1,12 +1,21 @@
 function solution(s){  
-  if(s.length == 0 || s.length == 1 || s[0] == ')' || s[s.length-1] == '(') return false
+  if(s[0] === ')' || s[s.length-1] === '(' || s.length === 1 || s.length === 0) return false
   let b = 0;
-  for(let i = 0; i < s.length; i++){ s[i] == '(' ? b += 1 : b -= 1; }
-  return b == 0 ? true : false;
+  for(let i = 0; i < s.length; i++){ 
+    if(s[i] === '('){
+      b+= 1;
+    }else{
+      b -= 1;
+      if(b < 0){
+        return false;
+      }
+    }
+  }
+  return b === 0 ? true : false;
 }
 
-console.log(solution("()()"),true)
+// console.log(solution("()()"),true)
 // console.log(solution("(())()"),true)
 // console.log(solution(")()("),false)
 // console.log(solution("(()("),false)
-
+console.log(solution("()())(()"),false)
