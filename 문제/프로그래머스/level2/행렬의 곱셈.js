22a232b184197d9c5cf4c2f000c0761b5d9c5a3c
km6293 <alstn7616@naver.com>
2022-12-28 19:21:30 +0900

    level2 미완

---
+++ 문제/프로그래머스/level2/행렬의 곱셈.js
@@ -0,0 +1,14 @@
+// function solution(arr1, arr2) {
+//   var answer = [];
+//   arr1.forEach((e,i) => {
+//     e.forEach((r,j) => {
+//       console.log(r, j, e, i )
+//     })
+//   })
+//   return answer;
+// }
+
+// console.log(solution([[1, 4], [3, 2], [4, 1]],[[3, 3], [3, 3]]), [[15, 15], [15, 15], [15, 15]])
+// // console.log(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]],[[5, 4, 3], [2, 4, 1], [3, 1, 1]]),[[22, 22, 11], [36, 28, 18], [29, 20, 14]])
+
+
