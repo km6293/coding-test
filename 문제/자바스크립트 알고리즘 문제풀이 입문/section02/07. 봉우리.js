bb04ec6ccfc20bcb2ea2b2ab54c0fad3dbbed0f9
km6293 <alstn7616@naver.com>
2023-01-09 22:01:54 +0900

    section02

--- 문제/자바스크립트 알고리즘 문제풀이 입문/section02/07. 봉우리.js
+++ 문제/자바스크립트 알고리즘 문제풀이 입문/section02/07. 봉우리.js
@@ -1,13 +1,14 @@
 function solution(arr){
+  // 왼 오 위 아래를 다 체크 하는 로직에서 i, j 가 가장자리(0이거나 arr.length-1)일 경우를 0으로 처리해서 제외시킴
   let cnt = 0;
   for(let i = 0; i < arr.length; i++){
     for(let j = 0; j < arr.length; j++){
-      console.log(i,j)
-      if(i != 0 && arr[i][j] < arr[i-1][j]) break;
-      if(i != arr.length-1 && arr[i][j] < arr[i+1][j]) break;
-      if(j != 0 && arr[i][j] < arr[i][j-1]) break;
-      if(j != arr.length-1 && arr[i][j] < arr[i][j+1]) break;
-      cnt+=1;
+      let [a,b,c,d] = [100,100,100,100];
+      j == arr.length-1 ? a = 0 : a = arr[i][j+1]
+      j == 0 ? b = 0 : b = arr[i][j-1]
+      i == arr.length-1  ? c = 0 : c = arr[i+1][j]
+      i == 0 ? d = 0 : d = arr[i-1][j]
+      if(a < arr[i][j] && b < arr[i][j] && c < arr[i][j] && d < arr[i][j]) cnt += 1;
     }
   }
   return cnt;
@@ -24,14 +25,3 @@ console.log(
       ]
     ),10
   )
-
-
-
-
-
-
-
-
-
-
-
