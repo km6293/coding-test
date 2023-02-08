050dced2599029544e6fbf829173d732be06808e
km6293 <alstn7616@naver.com>
2023-02-08 21:12:34 +0900

    section06

--- 문제/자바스크립트 알고리즘 문제풀이 입문/section06/06. 공주 구하기.js
+++ 문제/자바스크립트 알고리즘 문제풀이 입문/section06/06. 공주 구하기.js
@@ -1,16 +1,12 @@
-function solution(num1, num2) {
-  let nArr = Array.from({ length: num1 }, (_, i) => i + 1);
-  // while(nArr.length == 1){
-  //   if()
-  //   nArr.splice(2, 1);
-  // }
-  let startNum = num2;
-  let chkVal = nArr.findLastIndex((e, i) => e % num2 == 0);
-
-  startNum = !nArr[chkVal + 1] ? nVrr[0] : nArr[chkVal + 1];
-  nArr = nArr.filter((e, i) => e % num2 != 0);
-
-  console.log(startNum, nArr);
+function solution(n, k){
+  let answer;
+  let queue=Array.from({length:n}, (v, i)=>i+1); 
+  while(queue.length){
+      for(let i=1; i<k; i++) queue.push(queue.shift());
+      queue.shift();
+      if(queue.length===1) answer=queue.shift();
+  }  
+  return answer;
 }
 
 console.log(solution(8, 3));
