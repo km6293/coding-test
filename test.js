00c77b19ce21dfb6d2e50726effc6d3773ef8384
km6293 <alstn7616@naver.com>
2023-03-11 23:34:14 +0900

    section09

--- test.js
+++ test.js
@@ -1,22 +1,2 @@
-function solution(n, k, arr, m){         
-  let answer=0;
-  let cnt = 0; 
-  function DFS(L, s, sum){
-    cnt += 1;
-     if(L===k){
-          if(sum%m===0) answer++;
-     }
-     else{
-         for(let i=s; i<n; i++){
-             DFS(L+1, i+1, sum+arr[i]);
-         }
-     }
-  }
-
-  DFS(0, 0, 0);
-  console.log(cnt)
-  return answer;
-}
-
-let arr=[2, 4, 5, 8, 12];
-console.log(solution(5, 3, arr, 6));
+let test = [1,2,3,4,5];
+console.log(test.slice(0,2))
