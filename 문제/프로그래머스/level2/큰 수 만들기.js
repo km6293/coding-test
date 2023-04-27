562731f9146e9a69c1c5e62d45b63ec2bebb62f6
km6293 <alstn7616@naver.com>
2023-04-27 12:06:56 +0700

    level2

--- 문제/프로그래머스/level2/큰 수 만들기.js
+++ 문제/프로그래머스/level2/큰 수 만들기.js
@@ -1,26 +1,22 @@
 function solution(arr,k){
-  let nArr = arr.split('')
-  let m = arr.length - k;
+  arr = arr.split('').reverse()
+  let stack = [];
 
-  function getCombinations(arr, selectNumber) {
-    const results = [];
-    if (selectNumber === 1) return arr.map((el) => [el]); 
-    arr.forEach((fixed, index, origin) => {
-      const rest = origin.slice(index + 1); 
-      const combinations = getCombinations(rest, selectNumber - 1); 
-      const attached = combinations.map((el) => [fixed, ...el]); 
-      results.push(...attached); 
-    });
-    return results;
+  while(arr.length && k > 0){
+    stack.push(arr.pop());
+    while(arr[arr.length-1] > stack[stack.length-1] && k > 0){
+      stack.pop()
+      k -= 1;
+    }
   }
 
-  let cArr = getCombinations(nArr, m);
-  let val = 0;
-  cArr.map(e => {
-    let num = parseInt(e.join(''));
-    if(num > val) val = num
-  })
-  return val.toString()
+  if( k != 0 ) stack = stack.slice(0, -k)
+
+  return stack.join('') + arr.reverse().join('')
 }
 
-console.log(solution("1924",2),"94");
+// console.log(solution("1924",2),"94");
+// console.log(solution("1231234",3),"3234");
+// console.log(solution("4177252841",4),"775841");
+console.log(solution("987654321",5),"9876");
+
