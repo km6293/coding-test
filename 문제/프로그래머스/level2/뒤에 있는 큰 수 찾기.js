68913ab8bc53a7ef19e476a6abac70b754ef2114
km6293 <alstn7616@naver.com>
2023-04-30 01:38:25 +0700

    level2 2번째 실패

--- 문제/프로그래머스/level2/뒤에 있는 큰 수 찾기.js
+++ 문제/프로그래머스/level2/뒤에 있는 큰 수 찾기.js
@@ -3,17 +3,22 @@ function solution(arr){
   let cArr = [];
   for(let i = arr.length-1 ; i >= 0; i--){
     let val = arr.pop();
-    let chk = cArr.findIndex(e => e > val);
-    cArr.unshift(val);
-    chk ? rArr.push(-1) : rArr.push(cArr[chk]);
-    console.log(cArr, rArr)
+    let chk = undefined;
+    for(let r = cArr.length-1; r >= 0; r--){
+      if(cArr[r] > val){
+        chk = cArr[r];
+        break;
+      }
+    }
+    cArr.push(val);
+    chk == undefined ? rArr.push(-1) : rArr.push(chk);
   }
-  // return rArr.reverse()
+  return rArr.reverse()
 }
 
-console.log(solution([2, 3, 3, 5]), [3, 5, 5, -1])
+// console.log(solution([2, 3, 3, 5]), [3, 5, 5, -1])
 // console.log(solution([9, 1, 5, 3, 6, 2]), [-1, 5, 6, 6, -1, -1])
-// console.log(solution([10, 1, 10, 2, 10, 3, 10, 10, 10, 11, 11, 11, 12]), [11, 10, 11, 10, 11, 10, 11, 11, 11, 12, 12, 12, -1])
+console.log(solution([10, 1, 10, 2, 10, 3, 10, 10, 10, 11, 11, 11, 12]), [11, 10, 11, 10, 11, 10, 11, 11, 11, 12, 12, 12, -1])
 
 	
 	
