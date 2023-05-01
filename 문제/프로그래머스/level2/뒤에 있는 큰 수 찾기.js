c962bf2ad570da2690a0351a4d656b589d63f995
km6293 <alstn7616@naver.com>
2023-05-01 13:35:52 +0700

    level2 재시도 완료

--- 문제/프로그래머스/level2/뒤에 있는 큰 수 찾기.js
+++ 문제/프로그래머스/level2/뒤에 있는 큰 수 찾기.js
@@ -1,24 +1,16 @@
+// 실패하여 풀이를 보게 됨.
 function solution(arr){
-  let rArr = [];
-  let cArr = [];
-  for(let i = arr.length-1 ; i >= 0; i--){
-    let val = arr.pop();
-    let chk = undefined;
-    for(let r = cArr.length-1; r >= 0; r--){
-      if(cArr[r] > val){
-        chk = cArr[r];
-        break;
-      }
+  let rArr = Array(arr.length).fill(-1);
+  let sArr = [];
+  for(let i = 0; i < arr.length; i++){
+    while(arr[i] > arr[sArr.at(-1)]){
+      rArr[sArr.pop()] = arr[i];
     }
-    cArr.push(val);
-    chk == undefined ? rArr.push(-1) : rArr.push(chk);
+    sArr.push(i);
   }
-  return rArr.reverse()
+  return rArr
 }
 
-// console.log(solution([2, 3, 3, 5]), [3, 5, 5, -1])
+console.log(solution([2, 3, 3, 5]), [3, 5, 5, -1])
 // console.log(solution([9, 1, 5, 3, 6, 2]), [-1, 5, 6, 6, -1, -1])
-console.log(solution([10, 1, 10, 2, 10, 3, 10, 10, 10, 11, 11, 11, 12]), [11, 10, 11, 10, 11, 10, 11, 11, 11, 12, 12, 12, -1])
-
-	
-	
+// console.log(solution([10, 1, 10, 2, 10, 3, 10, 10, 10, 11, 11, 11, 12]), [11, 10, 11, 10, 11, 10, 11, 11, 11, 12, 12, 12, -1])
