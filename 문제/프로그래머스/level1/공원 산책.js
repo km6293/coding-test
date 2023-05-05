3012bed45ccd4e28c094a44923a7a41baacbc472
km6293 <alstn7616@naver.com>
2023-05-05 23:20:10 +0900

    level1

--- 문제/프로그래머스/level1/공원 산책.js
+++ 문제/프로그래머스/level1/공원 산책.js
@@ -1,44 +1,48 @@
 function solution(park, routes) {
-  // 높이, 넓이
-  let area = [park[0].length ,park.length ];
-  let [location, obstacle] = [[],[]];
-  park.forEach((e,i) => {
-    for(let r = 0; r < e.length; r++){
-      if(park[i][r] == 'X') obstacle.push([i,r]);
-      if(park[i][r] == 'S') location = [i,r];
+  let [height, width] = [park.length-1, park[0].length-1];
+  let row = park.findIndex(e => e.includes("S"));
+  let col = park[row].indexOf("S");
+  
+  for(let i = 0; i < routes.length; i++){
+    let [direction, move] = routes[i].split(' ');
+    let [tempRow, tempCol] = [row, col];
+    let chk = true;
+    for(let r = 0; r < move; r++){
+      if(direction == 'E') tempCol += 1;
+      if(direction == 'W') tempCol -= 1;
+      if(direction == 'S') tempRow += 1;
+      if(direction == 'N') tempRow -= 1;
+
+      if(tempRow < 0 || tempCol < 0 || tempRow > height || tempCol > width || park[tempRow][tempCol] == 'X') chk = false;
+      if(!chk) break
     }
-  })
-
-  for(let i = 0 ; i < routes.length; i++){
-    let val = routes[i].split(' ');
-    let [direction, move] = [val[0], parseInt(val[1])];
-    let moveVal = [...location];
-    let [max, min] = [0,0];
-    let x = -1;
-    if(direction == "E" || direction == "W"){
-      direction == "E" ? moveVal[1] += move : moveVal[1] -= move
-      if(0 <= moveVal[1] && area[1] > moveVal[1]){
-
-      }else{
-        max = Math.max([moveVal[1],location[1]])
-        min = Math.min([moveVal[1],location[1]])
-        x = obstacle.find(e => (e[0] == area[0]) && (e[1] <= max) && (e[1] >= min))
-      }
-    }else{
-      direction == "S" ? moveVal[0] += move : moveVal[0] -= move
-      if(0 <= moveVal[0] && area[0] > moveVal[0]){
-        
-      }else{
-        max = Math.max([moveVal[0],location[0]])
-        min = Math.min([moveVal[0],location[0]])
-        x = obstacle.find(e => (e[0] == area[0]) && (e[1] <= max) && (e[1] >= min))
-      }
-    }   
-    if(x != -1) location = [...moveVal]
+    if(chk){
+      row = tempRow;
+      col = tempCol;
+    }
   }
-  return location
+  return [row, col]
 }
 
-console.log(solution(["SOO","OOO","OOO"],["E 2","S 2","W 1"]), [2,1])
-// console.log(solution(["OSO","OOO","OXO","OOO"],["E 2","S 3","W 1"]), [0,0])
-	
+// console.log(solution(["SOO","OOO","OOO"], ["E 2","S 2","W 1"])); // [2,1]
+// console.log(solution(["SOO","OXX","OOO"], ["E 2","S 2","W 1"])); // [0,1]
+// console.log(solution(["OSO","OOO","OXO","OOO"], ["E 2","S 3","W 1"])); // [0,0]
+
+// console.log(solution(["OSO","OOO","OOO","OOO"], ["W 2"])); // [0,1]
+console.log(solution(["OSO","OOO","OOO","OOO"], ["N 2"])); // [0,1]
+// console.log(solution(["OSO","OOO","OOO","OOO"], ["S 5"])); // [0,1]
+// console.log(solution(["OSO","OOO","OOO","OOO"], ["E 5"])); // [0,1]
+
+// console.log(solution(["XXX","XSX","XXX"], ["E 1"])); // [1,1]
+// console.log(solution(["XXX","XSX","XXX"], ["W 1"])); // [1,1]
+// console.log(solution(["XXX","XSX","XXX"], ["S 1"])); // [1,1]
+// console.log(solution(["XXX","XSX","XXX"], ["N 1"])); // [1,1]
+
+// console.log(solution(["SOXOO","OOOOO","OOOOO", "OOOOO", "OOOOO"], ["E 3"])); // [0, 0]
+// console.log(solution(["SOOOX","OOOOO","OOOOO", "OOOOO", "OOOOO"], ["E 3"])); // [0, 3]
+// console.log(solution(["XOOOS","OOOOO","OOOOO", "OOOOO", "OOOOO"], ["W 3"])); // [0, 1]
+// console.log(solution(["OOXOS","OOOOO","OOOOO", "OOOOO", "OOOOO"], ["W 3"])); // [0, 4]
+// console.log(solution(["SOOOO","OOOOO","XOOOO", "OOOOO", "OOOOO"], ["S 3"])); // [0, 0]
+// console.log(solution(["SOOOO","OOOOO","OOOOO", "OOOOO", "XOOOO"], ["S 3"])); // [3, 0]
+// console.log(solution(["OOOOO","OOOOO","XOOOO", "OOOOO", "SOOOO"], ["N 3"])); // [4, 0]
+// console.log(solution(["XOOOO","OOOOO","OOOOO", "OOOOO", "SOOOO"], ["N 3"])); // [1, 0]
