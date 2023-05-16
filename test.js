51f58ac1be43d216944e904f53721087221dc90b
km6293 <alstn7616@naver.com>
2023-05-16 22:19:20 +0900

    부스트캠프 문제 테스트

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
-let a = 11;
-let b = a.toString(3);
-console.log(b)
+function solution(arr){
+  let obj = {};
+  let rArr = [];
+
+  arr.forEach(e => {
+    obj[e] == undefined ? obj[e] = 1 : obj[e] += 1;
+  })
+
+  for(let a in obj){
+    if(obj[a] != 1) rArr.push(obj[a]);
+  }
+  
+  return rArr
+}
+
+console.log(solution([1,2,3,3,3,3,4,4]))
+// console.log(solution([3,2,4,4,2,5,2,5,5]))
+// console.log(solution([3,5,7,9,1]))
