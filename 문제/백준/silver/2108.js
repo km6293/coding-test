56fe6ffa289404610c87f851a269144d5980f92f
km6293 <alstn7616@naver.com>
2023-06-05 20:03:22 +0900

    silver

--- 문제/백준/silver/2108.js
+++ 문제/백준/silver/2108.js
@@ -1,44 +1,25 @@
-let input = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split("\n");
-// let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+// let input = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split("\n").map(e => parseInt(e));
+let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n").map(e => parseInt(e));
 
 input.shift();
+input = input.sort((a,b) => a - b);
 
-let obj = {}
-input = input.map(e => {
-  obj[e] == undefined ? obj[e] = 1 : obj[e] += 1;
-  return parseInt(e);
-}).sort((a,b) => a - b);
+// 산술평균
+console.log(parseInt(Math.round(input.reduce((a,b) => a + b) / input.length)))
 
-// console.log(parseInt(input.reduce((a, b) => a + b) / input.length));
-// console.log(input[parseInt(input.length/2)]);
+// 중앙값
+console.log(input[Math.floor(input.length/2)])
 
+// 최빈값
+let obj = {};
+input.forEach(e => obj[e] == undefined ? obj[e] = 1 : obj[e] += 1 );
 let max = Math.max(...Object.values(obj));
-let cnt = 0;
-if([...Object.values(obj)].filter(e => e == max).length >= 2){
-  for(val in obj){
-    // console.log(obj[val])
-    if(max == obj[val]){
-      
-    }
-  }
+let maxList = [];
+for(const [key, value] of Object.entries(obj)){
+  if(value == max)maxList.push(parseInt(key));
 }
-// console.log(Object.values(obj).find(e => e == max))
+maxList.sort((a,b) => a - b);
+console.log(maxList.length > 1 ? maxList[1] : maxList[0]);
 
-for( const [key, value] of Object.entries(obj)){
-  console.log(key, value)
-  
-}
-
-
-// console.log(input.length == 1 ? 0 : input[input.length-1] - input[0]);
-
-// 산술평균 : N개의 수들의 합을 N으로 나눈 값
-// 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
-// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
-
-
-// 첫째 줄에는 산술평균을 출력한다. 소수점 이하 첫째 자리에서 반올림한 값을 출력한다.
-// 둘째 줄에는 중앙값을 출력한다.
-// 셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
-// 넷째 줄에는 범위를 출력한다.
+// 범위
+console.log(input[input.length -1 ] - input[0]);
