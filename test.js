77cb811375d447e0e7c77057f856d7ef5c17e761
km6293 <alstn7616@naver.com>
2023-06-14 21:57:51 +0900

    test

--- test.js
+++ test.js
@@ -1,18 +1,12 @@
-function solution(arr){
-  let obj = {};
-  let rArr = [];
+let d = 1;
+let e = 2;
 
-  arr.forEach(e => {
-    obj[e] == undefined ? obj[e] = 1 : obj[e] += 1;
-  })
+console.log(d, e);
+swap(d, e)
+console.log(d, e);
 
-  for(let a in obj){
-    if(obj[a] != 1) rArr.push(obj[a]);
-  }
-  
-  return rArr
+function swap (a, b){
+  let tmp = a;
+  d = b;
+  e = a;
 }
-
-console.log(solution([1,2,3,3,3,3,4,4]))
-// console.log(solution([3,2,4,4,2,5,2,5,5]))
-// console.log(solution([3,5,7,9,1]))
