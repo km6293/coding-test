cfe5bcebd7899b3f3b4b60db552cc95f20d7022e
km6293 <alstn7616@naver.com>
2023-06-17 12:13:17 +0900

    silver

---
+++ 문제/백준/silver/2161.js
@@ -0,0 +1,15 @@
+const n = parseInt(require('fs').readFileSync(__dirname + "/../input.txt").toString().trim());
+// const n = parseInt(require('fs').readFileSync("/dev/stdin").toString().trim());
+
+let [nArr, rArr] = [[],[]];
+for(let i = 1; i <= n; i++) nArr.push(i);
+nArr.reverse();
+
+while(nArr.length != 1){
+  rArr.push(nArr.pop());    
+  nArr.unshift(nArr.pop());
+}
+
+console.log(...rArr, ...nArr)
+
+
