fde8b9b89f2ce38477aa1aba0455f87f08d98843
km6293 <alstn7616@naver.com>
2023-06-18 22:08:53 +0900

    test

--- test.js
+++ test.js
@@ -1,34 +1,20 @@
-let val = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]];
-let map = {};
+const arr = [
+  ["aaa", "12:50", "20"],
+  ["bbb", "12:10", "30"],
+  ["ccc", "12:40", "10"]
+];
 
-val.forEach(e => {
-  e.forEach((r,i) => {
-    if(map[r] == undefined) map[r] = [];
-    map[r].push(!i ? e[1] : e[0]);
-  })
-})
-
-let maxLength = 0;
-let maxCnt = 0;
-let valueList = [...Object.values(map)];
-
-valueList.forEach(e => { if(e.length > maxLength) maxLength = e.length; })
-valueList.forEach(e => { if(maxLength == e.length) maxCnt += 1; })
-
-if(maxCnt == 1){
-  let val = [];
-  valueList.forEach(e => { if(maxLength == e.length) val = [...e] })
-
-  let rVal = new Array(val.length).fill(0);
-  let index = 0;
-  while(val.length != 0){
-    let nd = val.shift();
-    if(map[nd].length == 1){
-      rVal[index] += 1;
-    }else{
-      
-    }
+arr.sort((a, b) => {
+  const timeA = a[1];
+  const timeB = b[1];
+  
+  if (timeA < timeB) {
+    return -1;
+  } else if (timeA > timeB) {
+    return 1;
+  } else {
+    return 0;
   }
-}else{
-  return -1;
-}
+});
+
+console.log(arr);
