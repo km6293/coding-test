7a715b87ac29545f16da92d2a3305052e7871b2f
km6293 <alstn7616@naver.com>
2023-09-25 17:38:31 +0900

    추가 풀이

--- 문제/프로그래머스/level2/올바른 괄호.js
+++ 문제/프로그래머스/level2/올바른 괄호.js
@@ -14,6 +14,18 @@ function solution(s){
   return b === 0 ? true : false;
 }
 
+
+// 2번째 풀이
+// function solution(s){
+//   let cnt = 0;
+//   for(let i = 0 ; i < s.length; i++){
+//       cnt = s[i] == '(' ? cnt + 1 : cnt - 1;
+//       if(cnt < 0) return false;
+//   }
+  
+//   return cnt === 0 ? true : false;
+// }
+
 // console.log(solution("()()"),true)
 // console.log(solution("(())()"),true)
 // console.log(solution(")()("),false)
--- 문제/프로그래머스/level2/프린터.js
+++ 문제/프로그래머스/level2/프린터.js
@@ -25,6 +25,45 @@ function solution(arr, num){
   return cnt
 }
 
+// 2번째 풀이
+// class Queue {
+//   constructor(){
+//     this.queue = [];
+//     this.front = 0;
+//     this.rear = 0;
+//     this.cnt = 0;
+//   }
+
+//   enqueue(value, chk){
+//     this.queue[this.rear++] = value;
+//     if(chk) this.cnt -= 1;
+//   }
+
+//   dequeue(){
+//     const value = this.queue[this.front];
+//     delete this.queue[this.front];
+//     this.front += 1;
+//     this.cnt += 1;
+//     return value;
+//   }
+// }
+
+// function solution(arr, num){
+
+//   let queue = new Queue()
+//   arr.forEach((e,i) => queue.enqueue([e, i], false));
+//   let sArr = [...arr].sort((a,b) => a - b);
+
+//   while(true){
+//     let [value, index] = queue.dequeue();
+//     let maxValue = sArr[sArr.length-1];
+//     if(index === num && value == maxValue) break;
+//     maxValue === value ? sArr.pop() : queue.enqueue([value, index], true);
+//   }
+//   return queue.cnt;
+
+// }
+
 // console.log(solution([2, 1, 3, 2], 2), 1)
 // console.log(solution([1, 2, 3], 2), 1)
 // console.log(solution([1, 1, 9, 1, 1, 1], 0), 5)
