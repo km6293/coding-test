128bb2076a96400b1e586a16a6b4235470ff5d54
km6293 <alstn7616@naver.com>
2023-09-29 15:21:45 +0900

    level2

--- 문제/프로그래머스/level2/가장 큰 수.js
+++ 문제/프로그래머스/level2/가장 큰 수.js
@@ -1,28 +1,47 @@
-function solution(s){
-  s = s.map(e => e.toString()).sort().reverse()
-  let numArr = []
-  for(let i = 9 ; i >= 0; i--){
-    numArr.push(s.filter(e => e[0] == i))
-  }
+// function solution(s){
+//   s = s.map(e => e.toString()).sort().reverse()
+//   let numArr = []
+//   for(let i = 9 ; i >= 0; i--){
+//     numArr.push(s.filter(e => e[0] == i))
+//   }
+//   let val = '';
+
+//   for(let i = 0; i < numArr.length; i++){
+//     if(numArr[i].length != 0){
+//       if(numArr[i].length == 1){
+//         val += numArr[i]
+//       }else{
+//         let nArr = numArr[i].sort((a,b) => (b+a) - (a+b))
+//         for(let r = 0; r < nArr.length; r++){
+//           val += nArr[r]
+//         }
+//       }
+//     }
+//   }
+
+//   if(parseInt(val) == 0) val = '0';
+//   return val
+// }
+
+
+// 2 번째 풀이
+function solution(numbers) {
+
+  let arr = Array.from({length: 10}, () => []);
+
+  numbers.forEach(e => arr[(e+'')[0]].push(e));
+
   let val = '';
+  arr.reverse().map(e => {
+    val += e.sort((a,b) => (b+''+a) - (a+''+b)).join('');
+  })
 
-  for(let i = 0; i < numArr.length; i++){
-    if(numArr[i].length != 0){
-      if(numArr[i].length == 1){
-        val += numArr[i]
-      }else{
-        let nArr = numArr[i].sort((a,b) => (b+a) - (a+b))
-        for(let r = 0; r < nArr.length; r++){
-          val += nArr[r]
-        }
-      }
-    }
-  }
-
-  if(parseInt(val) == 0) val = '0';
-  return val
+  return parseInt(val) === 0 ? '0' : val;
 }
 
+
+
+// console.log(solution([555, 565, 566, 55, 56, 5, 54, 544, 549]))
 console.log(solution([0,0,0,0]),0)
 // console.log(solution([6, 10, 2]), "6210")
 // console.log(solution([3, 30, 34, 340, 5, 9, 32]), "9534330")
--- 문제/프로그래머스/level2/큰 수 만들기.js
+++ 문제/프로그래머스/level2/큰 수 만들기.js
@@ -1,22 +1,47 @@
-function solution(arr,k){
-  arr = arr.split('').reverse()
-  let stack = [];
+// N이 백만이면 O(N), O(N log N)
 
-  while(arr.length && k > 0){
-    stack.push(arr.pop());
-    while(arr[arr.length-1] > stack[stack.length-1] && k > 0){
-      stack.pop()
-      k -= 1;
+// function solution(arr,k){
+//   arr = arr.split('').reverse();
+//   let stack = [];
+
+//   while(arr.length && k > 0){
+//     stack.push(arr.pop());
+//     while(arr[arr.length-1] > stack[stack.length-1] && k > 0){
+//       stack.pop()
+//       k -= 1;
+//     }
+//   }
+
+//   if( k != 0 ) stack = stack.slice(0, -k)
+
+//   return stack.join('') + arr.reverse().join('')
+// }
+
+// 센세의 풀이
+
+function solution(number, k){
+  const stack = [];
+  let count = 0;
+
+  for (const item of number){
+    while(count < k && stack[stack.length - 1] < item){
+      stack.pop();
+      count += 1;
     }
+    stack.push(item);
   }
 
-  if( k != 0 ) stack = stack.slice(0, -k)
+  while(count < k){
+    stack.pop();
+    count += 1;
+  }
 
-  return stack.join('') + arr.reverse().join('')
+  return stack.join("");
 }
+
 
 // console.log(solution("1924",2),"94");
 // console.log(solution("1231234",3),"3234");
-// console.log(solution("4177252841",4),"775841");
-console.log(solution("987654321",5),"9876");
+console.log(solution("4177252841",4),"775841");
+// console.log(solution("987654321",5),"9876");
 
