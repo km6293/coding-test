2bbe140ccfb7526080e438386467936aa73af06c
km6293 <alstn7616@naver.com>
2023-10-16 15:01:38 +0900

    bronze

--- 문제/백준/1. bronze/19532.js
+++ 문제/백준/1. bronze/19532.js
@@ -1,17 +1,26 @@
 let [a,b,c,d,e,f] = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split(' ').map(Number);
 // let [a,b,c,d,e,f] = require('fs').readFileSync("/dev/stdin").toString().trim().split(' ').map(Number);
 
-// 값이 0일 경우가 있다.. 
+// 조건 -999 ~ 999이기 때문에 값이 0일 경우가 있다. 
+
+// 풀이 1. 완전탐색 (레이블문)
+// loop : for(let i = -999; i <= 999; i++){
+//   for(let j = -999; j <= 999; j++){
+//     if( (a * i) + (b * j) === c && (d * i) + (e * j) === f ){
+//       console.log(i, j);
+//       break loop;
+//     }
+//   }
+// }
+
+
+// 풀이 2. 연립방정식
+// 띄워쓰기를 안 쓰면 틀린다.
 // let y = ((c * d) - (f * a)) / ((b * d) - (e * a));
 // let x = ((c * e) - (f * b)) / ((e * a) - (b * d));
+// console.log(x+' '+y) 
 
-for(let i = -999; i <= 999; i++){
-  for(let j = -999; j <= 999; j++){
-    if( (a * i) + (b * j) === c && (d * i) + (e * j) === f ){
-      console.log(i, j);
-      return;
-    }
-  }
-}
+// const [a,b,c,d,e,f]=(require('fs').readFileSync(__dirname + "/../input.txt")+'').split(' ');
+// console.log(`${(c*e-b*f)/(a*e-b*d)} ${(a*f-c*d)/(a*e-b*d)}`);
 
-console.log(x, y);
+
