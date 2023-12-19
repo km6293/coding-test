1c9cde74b5f2004dc9d03a273eccb2c65871fd9e
km6293 <69450660+km6293@users.noreply.github.com>
2023-12-19 14:19:32 +0900

    silver

--- 문제/백준/2. silver/7568.js
+++ 문제/백준/2. silver/7568.js
@@ -1,15 +1,34 @@
 // let input = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split("\n");
-let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
-input.shift();
+// let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+// input.shift();
 
-let numList = Array(input.length).fill(1);
-for(let i = 0; i < input.length; i++){
-  let [aKg, aCm] = input[i].split(' ');
-  for(let r = 0; r < input.length; r++){
-    if(i != r){
-      let [bKg, bCm] = input[r].split(' ');
-      if(parseInt(aKg) < parseInt(bKg) && parseInt(aCm) < parseInt(bCm)) numList[i] += 1;
-    }
+// let numList = Array(input.length).fill(1);
+// for(let i = 0; i < input.length; i++){
+//   let [aKg, aCm] = input[i].split(' ');
+//   for(let r = 0; r < input.length; r++){
+//     if(i != r){
+//       let [bKg, bCm] = input[r].split(' ');
+//       if(parseInt(aKg) < parseInt(bKg) && parseInt(aCm) < parseInt(bCm)) numList[i] += 1;
+//     }
+//   }
+// }
+// console.log(numList.join(' '));
+
+
+// ==============================================================================
+
+let [N, ...list] = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split("\n");
+// let [N, ...list] = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let input = list.map(e => e.split(' ').map(Number));
+let ranking = [];
+
+for(let i = 0 ; i < input.length; i++){
+  let cnt = 1;
+  for(let j = 0; j < input.length; j++){
+    if(i !== j && input[i][0] < input[j][0] && input[i][1] < input[j][1] ) cnt += 1;
   }
+  ranking.push(cnt)
 }
-console.log(numList.join(' '));
+
+console.log(ranking.join(' '));
